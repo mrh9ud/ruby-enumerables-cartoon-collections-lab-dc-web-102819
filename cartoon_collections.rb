af9df072500fa9bbebd02aad75a6ae699da05f01
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-23 17:11:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,7 @@ def long_planeteer_calls(array_of_calls)
   
 end
 
-def find_the_cheese
-  
-  
+def find_the_cheese(array_of_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-
+  
 end
